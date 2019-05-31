5ca47d982b9e3b1e7eab3e15bab61bc55718e8d2
lucasg1 <lucas97bg@gmail.com>
2019-05-30 23:29:57 -0300

    tab1 pronta

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
+import { HttpClient } from '@angular/common/http';
+import { format } from 'util';
 
 @Component({
   selector: 'app-tab1',
@@ -7,13 +9,22 @@ import { AuthenticationService } from '../services/authentication.service';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  auxTitle: string;
+  auxData: string;
   campo: Array<{data: string, title: string, tag: string, evento:string}>;
   currentDATA: string;
   meses: Array<{mes:string, mes_num: string}>;
   
-  constructor(private auth: AuthenticationService) {
-    this.campo = [];  
+  constructor(private auth: AuthenticationService, private httpClient: HttpClient) {
+    this.httpClient.get('http://localhost:3000' + '/alldatatab1').subscribe((res)=>{
+    //console.log(res);
+
+    this.campo = []
+    for(var index in res){
+      //console.log(line);
+      this.campo.push({data: res[index].data, title: res[index].title, tag: res[index].tag, evento: res[index].evento});
+    }
+  });
     this.currentDATA = this.RetornaDataHoraAtual();
       this.meses = [
         {mes: "Jan", mes_num: "1"},
@@ -37,6 +48,40 @@ RetornaDataHoraAtual(){
   return localdate;
 }
 
+formataAtualiza(item){
+  var tempData = this.formatar(item)
+  this.atualizar(item)
+  return tempData
+}
+
+atualizar(item){
+  console.log('rodou atualizar com data: '+ item.data)
+  this.httpClient.put('http://localhost:3000/updatedatalinetab1/'+this.auxData+'/'+item.title,{
+    data: this.formatar(item),
+    title: item.title,
+  }).subscribe(
+    res=>{console.log(res);},
+    err=>{console.log('Error ocurred at update line')}
+  );
+}
+
+formatar(item){
+  console.log('rodou o formatar com data: '+ item.data)
+  var aux= new Date(item.data);
+  var dataformatada = (aux.getFullYear()) + '-' + (aux.getMonth()+1) + '-' + aux.getDate();
+
+return dataformatada;
+}
+
+updateLine(item){
+  this.httpClient.put('http://localhost:3000/updatetitlelinetab1/'+item.data+'/'+this.auxTitle,{
+    title: item.title,
+  }).subscribe(
+    res=>{console.log(res);},
+    err=>{console.log('Error ocurred at update line')}
+  );
+}
+
 atualiza(mes,dataAno){
   var ANO = new Date(dataAno);
   var an = ANO.getFullYear();
@@ -58,10 +103,26 @@ adicionar(mes,ano){
   var ANO = new Date(ano);
   var an = ANO.getFullYear();
   var data = an + '-' + (mes) + '-' + '1';
-      this.campo.push({ data: data, title: "", tag: "", evento: ""});
+  this.campo.push({ data: data, title: "", tag: "", evento: ""});
+  this.httpClient.post('http://localhost:3000/createlinetab1',{
+    data: data,
+    title: "",
+    tag: "",
+    evento: ""
+})
+  .subscribe(
+    res=>{console.log(res);},
+    err=>{
+      console.log('Error ocurred at adicionarCampo in tab1')
+    }
+  );
 }
   
 deletar(currencie){
+  this.httpClient.delete('http://localhost:3000/deletelinetab1/'+currencie.data+'/'+currencie.title).subscribe(
+    () => console.log('Deletando a linha do dia'+currencie.data+' e com title '+currencie.title),
+    (err) => console.log(err)
+  );
   this.campo.splice(this.campo.indexOf(currencie),1);
 }
 
